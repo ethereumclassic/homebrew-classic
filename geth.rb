b487b45c20a0af5721221752eaf78d239582abac
ia <isaac.ardis@gmail.com>
2017-11-02 12:07:29 -0500

    problem: gopath/root regarding go build relation
    solution: use gopath as buildpath

--- geth.rb
+++ geth.rb
@@ -12,9 +12,11 @@ class Geth < Formula
   depends_on 'go' => :build
 
   def install
-    ENV["GOROOT"] = "#{HOMEBREW_PREFIX}/opt/go/libexec"
+    ENV["GOPATH"] = buildpath
+    mkdir_p buildpath/"src/github.com/ethereumproject/"
+    ln_sf buildpath, buildpath/"src/github.com/ethereumproject/go-ethereum"
     system "go", "env" # Debug env
-    system "go", "build", "-o", "geth", "cmd/geth"
+    system "go", "build", "-o", "geth", "./cmd/geth"
     bin.install 'geth'
   end
 
