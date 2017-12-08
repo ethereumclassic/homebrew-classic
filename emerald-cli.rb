ee3f4a0d6531ffa894492d3dec3ffd7c21a5ebff
ia <isaac.ardis@gmail.com>
2017-12-08 16:11:40 -0600

    fix: wrong version number

--- emerald-cli.rb
+++ emerald-cli.rb
@@ -4,7 +4,7 @@ class EmeraldCli < Formula
   homepage 'https://github.com/ethereumproject/emerald-cli'
   # https://github.com/ethereumproject/emerald-cli/releases/download/v0.18.0/emerald-cli-osx-v0.18.1-832bba6.zip
   url "https://github.com/ethereumproject/emerald-cli/releases/download/v0.18.0/emerald-cli-osx-v0.18.1-832bba6.zip"
-  version "4.1.2"
+  version "0.18.1"
   # eg. openssl sha -sha256 geth-classic-osx-v4.1.1.tar.gz
   sha256 "626cffc48998ea008c8997b96cfa448a77d8636fb270f56ef6a50bfbaaf911bb"
 
