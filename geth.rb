1f1ce57758fb8cb625fffb2fa62f58d05cc51499
ia <isaac.ardis@gmail.com>
2018-02-07 20:08:12 +0900

    geth -> 4.2.2

--- geth.rb
+++ geth.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Geth < Formula
   homepage 'https://github.com/ethereumproject/go-ethereum'
-  url "https://github.com/ethereumproject/go-ethereum/releases/download/v4.2.1/geth-classic-osx-v4.2.1-d3deb0e.tar.gz"
-  version "4.2.1"
-  sha256 "12c75d1e023580a7f730d12a520fa3d131740b0266180e97820297cc51aa8127"
+  url "https://github.com/ethereumproject/go-ethereum/releases/download/v4.2.2/geth-classic-osx-v4.2.2-c127446.tar.gz"
+  version "4.2.2"
+  sha256 "da37aaae6d94c09aef8f599c570f63e1e815362039a67bfb7246b719bb459041"
 
   def install
     bin.install 'geth'
