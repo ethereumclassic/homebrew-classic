6c78c8acc70e6a3cdcdec9ff509bd714cccff114
ia <isaac.ardis@gmail.com>
2018-05-09 13:37:00 +0900

    Emerald-CLI 0.22.0

--- emerald-cli.rb
+++ emerald-cli.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class EmeraldCli < Formula
   homepage 'https://github.com/ethereumproject/emerald-cli'
-  url "https://github.com/ETCDEVTeam/emerald-cli/releases/download/v0.21.0/emerald-cli-v0.21.0-OSX-x86_64.tar.gz"
-  version "0.21.0"
-  sha256 "ce1de80b80e21a64baf7dc260f4f5aead4951fe38594b50567a37efc9d80a1e7"
+  url "https://github.com/ETCDEVTeam/emerald-cli/releases/download/v0.22.0/emerald-cli-osx-v0.22.0-5dbb148.zip"
+  version "0.22.0"
+  sha256 "06d82c0d5b3db72e12ffbc5647b1e52f3b32996bd619bf3649f419ff053e379e"
 
   def install
     bin.install 'emerald'
