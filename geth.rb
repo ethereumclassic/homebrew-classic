39f5c7bc5602b8f9538af9222062888128248758
ia <isaac.ardis@gmail.com>
2018-06-06 12:39:57 +0200

    geth v5.4.1
    note also that downloads url now uses build.etcdevteam.com

--- geth.rb
+++ geth.rb
@@ -2,9 +2,10 @@ require 'formula'
 
 class Geth < Formula
   homepage 'https://github.com/ethereumproject/go-ethereum'
-  url "https://github.com/ethereumproject/go-ethereum/releases/download/v5.4.0/geth-classic-osx-v5.4.0-4851b12.tar.gz"
-  version "5.4.0"
-  sha256 "77d87228f3207a9305badf30c0897c4b85ded3a0c5d14ba52d91619711e60905"
+  url "http://builds.etcdevteam.com/go-ethereum/v5.4.x/geth-classic-osx-v5.4.1-a3d20f6.tar.gz"
+  version "5.4.1"
+  sha256 "2b1375df5a7624ce24ff5f98ff1d2905ac499cb375315c0fd0fb69f5792791ab"
+
 
   def install
     bin.install 'geth'
