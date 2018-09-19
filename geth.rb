26d77a7094c463c2c7b2d19af1e3a6431884c05b
ia <isaac.ardis@gmail.com>
2018-09-19 10:26:36 -0500

    bump geth version -> 5.5.1

--- geth.rb
+++ geth.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Geth < Formula
   homepage 'https://github.com/ethereumproject/go-ethereum'
-  url "http://builds.etcdevteam.com/go-ethereum/v5.5.x/geth-classic-osx-v5.5.0-382aa8f.tar.gz"
-  version "5.5.0"
-  sha256 "74ca78979c647fc15007a85453f05196105a4512cfbb27b8587f4b17fb7d84a5"
+  url "https://github.com/ethereumproject/go-ethereum/releases/download/v5.5.1/geth-classic-osx-v5.5.1-8a3bc2d.tar.gz"
+  version "5.5.1"
+  sha256 "6a78520e5f99548bb8efc54efe7bcca2bafb99db6c8bb72bd61d4c9cc2bcbc25"
 
 
   def install
